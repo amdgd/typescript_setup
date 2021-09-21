d8985c9fe154274af0f1fa4935d9e9604d36352a
Ankit Maheshwari <maheshwari.ankit@gmail.com>
2021-09-21 21:00:18 +0530

    Added Stylelint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.css'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as bootstrap from 'bootstrap'
-
 console.log('Hello from the world of Console!')
